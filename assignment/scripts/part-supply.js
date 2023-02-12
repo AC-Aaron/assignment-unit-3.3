7ac74b2522293e11417b2530d4c9d94023bd9d25
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-12 15:38:58 -0600

    added stretch goals, fixed submission requirements

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -12,7 +12,7 @@ console.log('1. Number of partsNeeded:', partsNeeded)
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
-let supplyChanges = ['3','5','-6','0','7','11'];
+let supplyChanges = [3,5,-6,0,7,11];
 console.log('2. Array of supplyChanges:');
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
@@ -48,10 +48,10 @@ for (x = 0; x < supplyChanges.length; x++){
         console.log("no change");//if result is 0
     }
     else if (supplyChanges[x] > 0){
-        console.log(`Added ${x} parts`);// if result is a positive #
+        console.log(`Added ${supplyChanges[x]} parts`);// if result is a positive #
     }
     else {
-        console.log(`Removed ${x} parts`);//if result is a negative #
+        console.log(`Removed ${supplyChanges[x]} parts`);//if result is a negative #
     }
 }
 
@@ -59,11 +59,32 @@ for (x = 0; x < supplyChanges.length; x++){
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+
+for(parts of supplyChanges){
+    console.log(parts);//showing each value in the supplyChanges array. 
+    if(parts==0) {
+        console.log("no change");//if result is 0
+    }
+    else if (parts > 0){
+        console.log(`Added ${parts} parts`);// if result is a positive #
+    }
+    else {
+        console.log(`Removed ${parts} parts`);//if result is a negative #
+    }
+} 
+
 console.log('7. Showing supplyChanges with "for of" loop');
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('8. Total supplies available is:');
+
+let sumOf = 0
+
+for (parts of supplyChanges){
+    sumOf = sumOf + parts;
+    
+}
+console.log('8. Total supplies available is:', sumOf);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -73,3 +94,12 @@ console.log('8. Total supplies available is:');
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
 
+let parts2 = 572 //parts amounts
+let box = 0 //#of boxes
+
+while(parts2>=7){   //so longe as parts are greater than 7
+    parts2 -= 7;    //subtrack 7 for each box filled
+    box++;          //keep track of # of boxes filled with 7 parts each
+}
+console.log('total boxes filled=', box);    //logging # of boxes filled
+console.log('parts left over=', parts2);    //logging # of parts leftover. 
